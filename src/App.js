6bc765937f7e41f36967f8b4a7cc611d4da9f40b
Americanman <snowboardracer7@gmail.com>
2021-02-07 00:31:05 -0500

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState }from 'react'
+import { getOneTodo } from './api'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function useRepos(id) {
+	const [repos, setRepos] = React.useState([])
+	const [loading, setLoading] = React.useState(true)
+
+	React.useEffect(() => {
+		setLoading(true)
+		getOneTodo(id).then(response => response.json())
+ 		 .then(json => { setRepos(json); setLoading(false) })
+	}, [id])
+
+	return [loading, repos, setRepos]
 }
 
+function App() {
+	const [id, setId] = useState(1)
+	const [loading, repos] = useRepos(id)
+	return (
+		<div>
+		{loading ? <h1>Loaidng</h1> :
+   		 <div className="App">
+		   	<button onClick={() => setId(id+1)}>+ 1 id</button>
+			<p>{JSON.stringify(repos)}</p>
+   		 </div>}
+		</div>)
+}
+	
 export default App;
---
+++ src/api.js
@@ -0,0 +1,1 @@
+export async function getOneTodo(id) { return await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)}
